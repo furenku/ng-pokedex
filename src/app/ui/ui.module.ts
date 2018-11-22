c3728a7715f3cb98ffc0aac483d59b9df70a161c
furenku <furenku@gmail.com>
2018-11-21 18:41:03 -0600

    show more button && header link

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     ReactiveFormsModule
   ],
   declarations: [HeaderComponent, FooterComponent],
